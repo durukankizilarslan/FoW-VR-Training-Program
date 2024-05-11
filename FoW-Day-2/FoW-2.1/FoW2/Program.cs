b6b1565a560fed522c0dada43cc3fa9bd85064a9
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-05-11 17:41:04 +0300

    FoW Day 2 content added

---
+++ FoW-Day-2/FoW-2.1/FoW2/Program.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoW2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //-----------------------------------------------------------------
+            //PRATIK 2
+            //If yapisi kullanarak hesap makinesi yap, 2 sayi iste islem iste
+            //1-Toplama, 2-Cikarma vs
+            //-----------------------------------------------------------------
+
+            //BAŞLIK
+            Console.WriteLine("================================");
+            Console.WriteLine("HESAP MAKİNESİNE HOŞ GELDİNİZ");
+            Console.WriteLine("================================ \n");
+
+            //1. sayi girisi
+            Console.WriteLine("Hoşgeldiniz! Bu bir hesap makinesidir. Lütfen 2 adet sayı giriniz.\n");
+            
+            //Sayi giris dongusu
+            while (true)
+            {
+                Console.Write("Birinci sayiyi girin: ");
+                double num1 = System.Convert.ToDouble(Console.ReadLine());
+
+                //2. sayi girisi
+                Console.Write("Ikinci sayiyi girin: ");
+                double num2 = System.Convert.ToDouble(Console.ReadLine());
+
+                //İşlem seçimi
+                Console.WriteLine("Yapmak istediğiniz işlemi seçin. İstediğiniz işlem için ilgili numaraya basın ve Enter'ı tuşlayın:");
+                Console.WriteLine("+ : Toplama \n- : Çıkarma \n* : Çarpma \n/ : Bölme \n");
+
+                char islemIstegi = System.Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("");
+
+                //İşlem Uygulanması
+                switch(islemIstegi)
+                {
+                        case '+': 
+                        Console.WriteLine("Sayıların toplamı: " + (num1 + num2) + "\n");
+                        break;
+
+                        case '-':
+                        Console.WriteLine("Sayıların farkı: " + (num1 - num2) + "\n");
+                        break;
+
+                        case '*':
+                        Console.WriteLine("Sayıların çarpımı: " + (num1 * num2) + "\n");
+                        break;
+
+                        case '/':
+                        Console.WriteLine("Sayıların bölümü: " + (num1 / num2) + "\n");
+                        break;
+
+                        //else durumu için default case kullan
+                        default:  Console.WriteLine("Geçersiz bir işlem karakteri seçtiniz");
+                        break;
+                        
+                }
+
+                //if (islemIstegi == 1)
+                //{
+                //    Console.WriteLine("Sayıların toplamı: " + (num1 + num2) + "\n");
+                //}
+                //else if (islemIstegi == 2)
+                //{
+                //    Console.WriteLine("Sayıların farkı: " + (num1 - num2) + "\n");
+                //}
+                //else if (islemIstegi == 3)
+                //{
+                //    Console.WriteLine("Sayıların çarpımı: " + (num1 * num2) + "\n");
+                //}
+                //else if (islemIstegi == 4)
+                //{
+                //    Console.WriteLine("Sayıların bölümü: " + (num1 / num2) + "\n");
+                //}
+
+
+
+
+                //İşlem Çıkış İsteği
+                Console.WriteLine("Uygulamadan çıkmak istiyor musunuz? \nÇıkmak İçin 'X' , Devam etmek için 'Y' tuşuna basınız:\n *Başka bir tuşa basmanız halinde işlemlere devam edilecektir \n \n İşlem isteğiniz (X/Y): ");
+                string bitisIstek = Console.ReadLine();
+
+                if (bitisIstek.ToLower() == "x") 
+                {
+                    break;
+                } else if (bitisIstek.ToLower() == "y") {
+                    Console.WriteLine("Hesaplara devam ediyoruz!");
+                } else
+                {
+                    Console.WriteLine("Geçersiz girdi seçtiniz, işlemlere devam ediyoruz.");
+                }
+            }
+
+            //-----------------------------------------------
+            //Problem 2
+            //-----------------------------------------------
+            //1 sayı alın. Girilen sayı 50 ile 100 arasındaysa ekrana o sayı kadar merhaba yazdırsın.
+            ////50'den küçük veya 100'den büyükse ekrana o sayının 6 ile bölümünden kalanı yazdırsın.
+
+            
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ FoW-Day-2/FoW-2.2/FoW-2.2/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace FoW_2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------------------------------------------
+            //Problem 2
+            //-----------------------------------------------
+            //1 sayı alın. Girilen sayı 50 ile 100 arasındaysa ekrana o sayı kadar merhaba yazdırsın.
+            //50'den küçük veya 100'den büyükse ekrana o sayının 6 ile bölümünden kalanı yazdırsın.
+
+            //Console.WriteLine("Bir sayı girin: ");
+
+            //double sayiGirdi = System.Convert.ToDouble(Console.ReadLine());
+
+            ////50 - 100 arası değerlerin çıktısı
+            //if (sayiGirdi > 50 && sayiGirdi < 100)
+            //{
+            //    //Sayı girdisinin 0'a eşit oluncaya kadar "Merhaba" yazdırılması
+            //    for (int i = 0; i < sayiGirdi; i++)
+            //    {
+            //        Console.WriteLine((i+1) + ".Merhaba");
+            //    }
+            //}
+            ////Girdi 50'den küçük veya 100'den büyük ise
+            //else if(sayiGirdi < 50 || sayiGirdi > 100)
+            //{
+            //    Console.WriteLine(sayiGirdi % 6);
+
+            //}
+
+
+            //-----------------------------------------------
+            //Problem 3
+            //-----------------------------------------------
+            //Kullanici 5 sayi isteyecek, toplam bu diye yazdiracak
+
+            double toplam = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                
+                Console.WriteLine((i + 1) + ". Sayıyı Girin");
+                double numInput = System.Convert.ToDouble(Console.ReadLine());
+                toplam += numInput;
+
+            }
+
+            Console.WriteLine("Sayilarınızın toplamı: " + toplam);
+
+
+            Console.Read();
+        }
+    }
+}

4147efd0edac37b6e619ad3ec16acb43667f62c2
Durukan Kizilarslan <durukizilarslan@gmail.com>
2024-05-25 14:43:39 +0300

    FoW Day 4 content added

---
+++ FoW-Day-4/FoW-4.1/FoW-4.1/Program.cs
@@ -0,0 +1,116 @@
+﻿using System.Linq.Expressions;
+
+namespace FoW_4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //================
+            //PROBLEM 1
+            //================
+
+
+            //// Liste ve döngü değişkeni olşturma
+            //List<string> girdiler = new List<string>();
+            //bool dongu = true;
+
+            //Console.WriteLine("===================\n   Hoşgeldiniz!\n===================\nListeniz oluşturulmuştur.\nLütfen listenize eklemek istediğiniz elemanı yazınız: ");
+
+            //while (dongu) // Girdi kabul edilen döngü
+            //{
+            //    string girdi = Console.ReadLine(); // Girdi kabulü ve listeye ekleme
+            //    Console.WriteLine("Eklediğiniz eleman: " + girdi);
+
+            //    if (girdi.ToLower() == "bitti") // Girdinin "bitti" olup olmadığını kontrol etme
+            //    {
+            //        dongu = false; // Döngüyü bitir
+            //        Console.WriteLine("Döngüyü bitirdiniz. İşte listeniz: ");
+
+            //        foreach (var item in girdiler) // Listeyi yazdır
+            //        {
+            //            Console.Write(item + " - ");
+            //        }
+            //    } else
+            //    {
+            //        girdiler.Add(girdi); // Girdiyi ekle
+            //        Console.WriteLine("Yeni eleman ekleyin. Eklemek istemiyorsanız 'bitti' yazın ve Enter'a basarak döngüden çıkın");
+            //    }
+
+
+            //}
+
+            //================
+            //PROBLEM 2
+            //================
+
+            //List<string> kitaplik = new List<string>();
+            ////1-Ekleme
+            ////2-Silme
+            ////3-Listeleme
+            ////4-Çıkış
+            ////switch-case
+
+            //bool dongu = true;
+
+            //while (dongu)
+            //{
+            //    Console.WriteLine("Hoşgeldiniz! Listeniz oluşturulmuştur. Listenizle alakalı yapmak istediğiniz işlemi seçiniz:\n1-Ekleme | 2-Silme | 3-Listeleme | 4-Arama | 5-Çıkış");
+            //    int istek = Convert.ToInt32(Console.ReadLine());
+
+            //    switch(istek) 
+            //    {
+            //        case 1:
+            //            Console.WriteLine("Eklemek istediginiz degeri girin:");
+            //            string eklenen = Console.ReadLine();
+            //            kitaplik.Add(eklenen);
+            //            Console.WriteLine("Ekeldiğiniz değer: " + eklenen + "\n");
+            //            break;
+
+            //        case 2:
+
+            //            if (kitaplik.Count == 0)
+            //            {
+            //                Console.WriteLine("Kitaplıkta silinecek bir şey yok");
+            //                break;
+            //            }
+
+            //            Console.WriteLine("Silmek istediginiz degerin listedeki numarasını girin:");
+            //            int silinen = Convert.ToInt32(Console.ReadLine());
+            //            kitaplik.RemoveAt(silinen - 1);
+            //            Console.WriteLine("\n");
+            //            break;
+
+            //        case 3:
+            //            Console.WriteLine("Listenizin çıktısı: ");
+            //            foreach (var item in kitaplik)
+            //            {
+            //                Console.Write(item + " - ");
+            //            }
+            //            Console.WriteLine("\n");
+            //            break;
+
+            //        case 4:
+            //            Console.WriteLine("Aradığınız kitabın ismini girin: ");
+            //            string aranan = Console.ReadLine();
+
+            //            break;
+
+            //        case 5:
+            //            Console.WriteLine("Uygulamadan çıkıyoruz:");
+            //            dongu = false;
+            //            break;
+            //    }                
+
+            //}
+
+
+
+
+            
+
+
+
+        }
+    }
+}
---
+++ FoW-Day-4/FoW-4.2/FoW-4.2/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace FoW_4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //---------------------------
+            //FONKSIYONLAR
+            //---------------------------
+
+
+            //====================
+            //PROBLEM 1
+            //Iki sayi sectir, bunlari fonksiyonlari kullanarak 4 islem yaptir
+            //====================
+
+            //Console.WriteLine("İşlemciye hoşgeldiniz. 2 sayı giriniz:");
+            //Console.Write("Sayi bir: ");
+            //double sayi1 = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Sayi iki: ");
+            //double sayi2 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Yapmak istediğiniz işlemi seçin: \n1 - Toplama | 2 - Çıkarma | 3 - Çarpma | 4 - Bölme");
+            //double secim = Convert.ToInt32(Console.ReadLine());
+
+            //switch(secim)
+            //{
+            //    case 1:
+            //        Topla(sayi1, sayi2); 
+            //        break;
+
+            //    case 2:
+            //        double sonuc = Cikar(sayi1, sayi2);
+            //        Console.Write(sonuc);
+            //        break;
+
+            //    case 3:
+            //        Carp(sayi1, sayi2);
+            //        break;
+
+            //    case 4:
+            //        Bol(sayi1, sayi2);
+            //        break;
+            //}
+
+            //Console.WriteLine("Sonuc: " +Carp(Topla(sayi1,sayi2), Cikar(sayi1, sayi2)));
+
+            //=================
+            //PROBLEM 2
+            //Iki islem arasi secim yaptir Yazdirma isterse merhaba yazdir, Toplama isterse iki sayi sectirip toplat
+            //=================
+
+            Console.WriteLine(("Merhaba. Yapilacak iki işlemden bir tanesini seç:\n1 - Yazdırma | 2 - Toplama"));
+
+            string secim = Console.ReadLine();
+
+
+            switch (secim)
+            {
+                case "1":
+                    Console.WriteLine("Merhabaaa");
+                    break;
+
+                case "2":
+                    Console.WriteLine();
+                    Topla();
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Topla()
+        {
+            Console.Write("Sayi bir: ");
+            double sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Sayi iki: ");
+            double sayi2 = Convert.ToDouble(Console.ReadLine());
+            string sonuc = Convert.ToString(sayi1 + sayi2);
+            Console.WriteLine("Toplamin sonucu: " + sonuc);
+        }
+
+        //private static double Cikar(double a, double b)
+        //{
+
+        //    double sonuc = a - b;
+        //    Console.WriteLine("Sayinin farki: " + sonuc);
+        //    return sonuc;
+        //}
+
+        //private static double Carp(double a, double b)
+        //{
+
+        //    double sonuc = a * b;
+        //    Console.WriteLine("Sayinin carpimi: " + sonuc);
+        //    return sonuc;
+        //}
+
+        //private static void Bol(double a, double b)
+        //{
+
+        //    double sonuc = a / b;
+        //    Console.WriteLine("Sayinin bolumu: " + sonuc);
+        //}
+
+        //Çıkarma, çarpma, bölme işlemleri yapılacak
+        //Ikı sayı isteyip işlem seçtirip fonksiyonlar üzerinden işlemi yaptır
+
+        
+
+    }
+}
